3aff61f955c005290ca0df6360b2f5a225c3b602
Jover Lee <joverlee521@gmail.com>
2019-04-01 12:01:17 -0700

    updated pattern clump finder to be more efficient

--- Bioinformatics/Replication_Origin/pattern_count_frequency.py
+++ Bioinformatics/Replication_Origin/pattern_count_frequency.py
@@ -1,3 +1,6 @@
+import urllib.request
+from collections import defaultdict
+
 def pattern_count(text, pattern):
     '''
     Counts the number of times a pattern appears in a text.
@@ -31,13 +34,10 @@ def frequency_map(text, k):
             key = k-length substrings of text
             value = count of substring within text
     '''
-    freq = {}
+    freq = defaultdict(lambda: 0)
     for i in range(len(text) - k + 1):
         pattern = text[i:i+k]
-        if pattern in freq:
-            freq[pattern] += 1
-        else:
-            freq[pattern] = 1
+        freq[pattern] += 1
     return freq
 
 def most_frequent_pattern(text, k):
@@ -74,3 +74,109 @@ def pattern_matching(text, pattern):
             positions.append(i)
     return positions
 
+def symbol_to_number(symbol):
+    '''
+    Convert nucleotides("A", "C", "G","T") to numbers(0, 1, 2, 3)
+    '''
+    for i, char in enumerate("ACGT"):
+        if symbol == char:
+            return i
+
+
+def pattern_to_number(pattern):
+    '''
+    Convert pattern to a number that represents the patterns index in a list of lexicographically ordered patterns
+
+    INPUT:
+        pattern(str): pattern to convert to number
+    
+    OUTPUT:
+        (int): integer representing the pattern
+    '''
+    if len(pattern) == 0:
+        return 0
+    symbol = pattern[len(pattern)-1]
+    prefix = pattern[:len(pattern)-1]
+    return 4 * pattern_to_number(prefix) + symbol_to_number(symbol)
+
+def number_to_symbol(number):
+    '''
+    Convert number(0,1,2,3) to nucleotide symbols("A", "C", "G", "T")
+    '''
+    for i, char in enumerate("ACGT"):
+        if number == i:
+            return char
+
+def number_to_pattern(number, k):
+    '''
+    Convert a number to a nucleotide pattern
+    
+    INPUT:
+        number(int)
+        k(int): the length of the pattern
+    
+    OUTPUT:
+        (str): pattern represented by number
+    '''
+    if k == 1:
+        return number_to_symbol(number)
+    prefix_number = number // 4
+    remainder = number % 4
+    prefix_pattern = number_to_pattern(prefix_number, k-1)
+    return prefix_pattern + number_to_symbol(remainder)
+
+def computing_frequencies(text, k):
+    '''
+    Creates a frequency list of all possible k length patterns within a text
+
+    INPUT:
+        text(str): the string to map through
+        k(int): the length of the substrings to find within text
+    
+    OUTPUT:
+        freq_array(lst): a list of all the counts of k-length patterns withing text,
+        with each index pointing to the number representing the pattern
+    '''
+    freq_array = [0] * (4**k)
+    for i in range(len(text) - k + 1):
+        pattern = text[i:i+k]
+        j = pattern_to_number(pattern)
+        freq_array[j] += 1
+    return freq_array
+
+def pattern_clump_finder(genome, k, L, t):
+    '''
+    Find kmers that form (L,t)-clumps in the genome. 
+    Meaning, we are finding kmer that appear at least t times within substring of length L in the genome
+
+    INPUT:
+        genome(str): the genome to be analyzed
+        k(int): the length of patterns to find within genome
+        L(int): the length of the substring that encompasses a clump
+        t(int): the number of times a kmer needs to appear within substring
+    
+    OUTPUT:
+        kmers(set): a set of kmers that form clumps within genome
+    '''
+    kmers = set()
+    substring = genome[:L]
+    freq_map = frequency_map(substring, k)
+    for key, value in freq_map.items():
+        if value >= t:
+            kmers.add(key)
+    for i in range(1, len(genome) - L + 1):
+        first_pattern = genome[i-1:i-1+k]
+        freq_map[first_pattern] -= 1
+        last_pattern = genome[i+L-k:i+L]
+        freq_map[last_pattern] += 1
+        if freq_map[last_pattern] >= t:
+            kmers.add(last_pattern)
+    return kmers
+
+
+text = urllib.request.urlopen("http://bioinformaticsalgorithms.com/data/realdatasets/Rearrangements/E_coli.txt").read()
+k = 9
+L = 500
+t = 3
+pattern_clump = pattern_clump_finder(text, k, L, t)
+print(len(pattern_clump))
